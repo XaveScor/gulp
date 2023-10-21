54b536f339855ab9c3ded17f2fcbe7a3e63eb71c
Andrew Vassilyev <xavescor@gmail.com>
2023-10-22 04:02:54 +0600

    26: port bach tests to vitest

--- src/bach/test/parallel.test.mjs
+++ src/bach/test/parallel.test.mjs
@@ -1,7 +1,6 @@
 import { disableDeprecationWarnings, resetDeprecationFlags } from '../../deprecation.mjs';
 import { parallel } from '../parallel.mjs';
-
-const { default: expect } = await import('expect');
+import { describe, test, expect, beforeEach } from 'vitest';
 
 function fn1(done) {
   done(null, 1);
@@ -26,26 +25,26 @@ describe('bach: parallel', function () {
     disableDeprecationWarnings();
     resetDeprecationFlags();
   });
-  it('should execute functions in parallel, passing results', async () => {
+  test('should execute functions in parallel, passing results', async () => {
     const [error, results] = await parallel([fn1, fn2, fn3]);
 
     expect(error).toEqual(null);
     expect(results).toEqual([1, 2, 3]);
   });
 
-  it('should execute functions in parallel, passing error', async () => {
+  test('should execute functions in parallel, passing error', async () => {
     const [error, results] = await parallel([fn1, fn3, fnError]);
 
-    expect(error).toBeAn(Error);
+    expect(error).toBeInstanceOf(Error);
     expect(results).toEqual([1, 3, undefined]);
   });
 
-  it('should take extension points and call them for each function', async () => {
+  test('should take extension points and call them for each function', async () => {
     const arr = [];
     const fns = [fn1, fn2, fn3];
     const [error] = await parallel([fn1, fn2, fn3], {
       create: function (fn, idx) {
-        expect(fns).toInclude(fn);
+        expect(fns.includes(fn)).toBeTruthy();
         arr[idx] = fn;
         return arr;
       },
--- src/bach/test/series.test.mjs
+++ src/bach/test/series.test.mjs
@@ -1,7 +1,7 @@
+import { describe, test, expect, beforeEach } from 'vitest';
+
 import { disableDeprecationWarnings, resetDeprecationFlags } from '../../deprecation.mjs';
 import { series } from '../series.mjs';
-
-const { default: expect } = await import('expect');
 
 function fnSync() {}
 
@@ -28,54 +28,54 @@ describe('bach: series', function () {
     disableDeprecationWarnings();
     resetDeprecationFlags();
   });
-  it('should can take the sync functions without return value', async () => {
+  test('should can take the sync functions without return value', async () => {
     const [error, results] = await series([fnSync]);
 
     expect(error).toEqual(null);
     expect(results).toEqual([undefined]);
   });
-  it('should can take the sync functions', async () => {
+  test('should can take the sync functions', async () => {
     const [error, results] = await series([fn1]);
 
     expect(error).toEqual(null);
     expect(results).toEqual([1]);
   });
-  it('should can take the async callback functions', async () => {
+  test('should can take the async callback functions', async () => {
     const [error, results] = await series([fn2]);
 
     expect(error).toEqual(null);
     expect(results).toEqual([2]);
   });
-  it('should can take the async promised functions', async () => {
+  test('should can take the async promised functions', async () => {
     const [error, results] = await series([fn3]);
 
     expect(error).toEqual(null);
     expect(results).toEqual([3]);
   });
-  it('should execute functions in series, passing results', async () => {
+  test('should execute functions in series, passing results', async () => {
     const [error, results] = await series([fn1, fn3]);
 
     expect(error).toEqual(null);
     expect(results).toEqual([1, 3]);
   });
 
-  it('should execute functions in series, passing error', async () => {
+  test('should execute functions in series, passing error', async () => {
     const [error] = await series([fnError]);
-    expect(error).toBeAn(Error);
+    expect(error).toBeInstanceOf(Error);
   });
 
-  it('should save the array size of results', async () => {
+  test('should save the array size of results', async () => {
     const [, results] = await series([fn1, fn3, fnError, fn2, fnError]);
 
     expect(results).toEqual([1, 3, undefined, undefined, undefined]);
   });
 
-  it('should take extension points and call them for each function', async () => {
+  test('should take extension points and call them for each function', async () => {
     const arr = [];
     const fns = [fn1, fn3];
     const [error] = await series([fn1, fn3], {
       create: function (fn, idx) {
-        expect(fns).toInclude(fn);
+        expect(fns.includes(fn)).toBeTruthy();
         arr[idx] = fn;
         return arr;
       },
